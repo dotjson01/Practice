ec80f8b00f6e51b5833e442c2bda87efa857010d
Sahil Ali <sahilali88827@gmail.com>
2024-02-12 15:45:48 +0530

    switch5 file added

---
+++ switch5.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+
+int main(){
+    int a=3,b=4,c;
+    c=b-a;
+    switch(c)
+    {
+        case 1 || 2:
+        printf("Good give me your number");
+        break;
+       case a||b: 
+        printf("Bad out my class");
+        break;
+    }
+
+    return 0;
+}
+
+
+#include<stdio.h>
+
+int main(){
+    int a=3,b=4,c;
+    c=b-a;
+    switch(c)
+    {
+        case 1 || 2:
+        printf("Good give me your number");
+        break;
+        case 3:
+        case 4:
+        printf("Bad out my class");
+        break;
+    }
+
+    return 0;
+}
