7aebf506a217c0115ce47c912db4872d838fe2f0
Sahil Ali <sahilali88827@gmail.com>
2024-02-27 00:21:21 +0530

    pointer_practise file updated

--- pointer_practise2.c
+++ pointer_practise2.c
@@ -6,7 +6,7 @@ int main()
     float *b, *c;
     b = &a; // the address of a is assigned to b . So, b now points to a 
     c = b; //The value of b (which is the address of a) is assigned to c. So, c now also points to a
-    printf("The address of a is %f\n", a);
+    printf("The address of a is %\n", &a);
     printf("The address of b is %u\n", b);
     printf("The address of c is %u\n", c);
     printf("%f %f %f %f %f\n", a, *(&a), *&a, *b, *c);
