4c76b57b7005249fef104c40b1826424a3cefab5
Sahil Ali <sahilali88827@gmail.com>
2024-03-01 14:08:58 +0530

    recursion practise file added

--- recursion_practise2.c
+++ recursion_practise2.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
+#include<stdlib.h>
 
 int main(){
     printf("I C , you C , we C , they C\n");                                   
-    // main();
+    main();
     return 0;
     }
