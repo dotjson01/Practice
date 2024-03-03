f3340bad88037bc38417fad676694d522cd805a5
Sahil Ali <sahilali88827@gmail.com>
2024-03-03 20:37:55 +0530

    printing 1 to 10

---
+++ python practise/printing_1_to_10.py
@@ -0,0 +1,7 @@
+# This is a for loop that iterates over the range of numbers from 1 to 10
+for i in range(1, 11):
+    # This is how to print each number without a newline character at the end
+    print(i, end=" ")
+
+# This is how to print a newline character after the loop
+print()
