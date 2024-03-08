c783f867495973484360f8cad905b4271779365c
Sahil Ali <sahilali88827@gmail.com>
2024-03-08 12:53:46 +0530

    kilo , tons, lb question in function

---
+++ python practise/tempCodeRunnerFile.py
@@ -0,0 +1,18 @@
+def convert_weight(weight_kg):
+#     weight_g = weight_kg * 1000
+#     weight_ton = weight_kg / 1000
+#     weight_lb = weight_kg * 2.20462
+
+#     return weight_g, weight_ton, weight_lb
+
+# def main():
+#     weight_kg = float(input("Enter the weight of the commodity in kilograms: "))
+    
+#     weight_g, weight_ton, weight_lb = convert_weight(weight_kg)
+    
+#     print("The equivalent weight in grams is: {weight_g} g")
+#     print("The equivalent weight in tons is: {weight_ton} tons")
+#     print("The equivalent weight in pounds is: {weight_lb} lbs")
+
+# if __name__ == "__main__":
+#     main()
