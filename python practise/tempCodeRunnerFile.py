a8948d42d2ade966fcd6c21cb91dfa69e262b4ec
Sahil Ali <sahilali88827@gmail.com>
2024-03-09 20:32:36 +0530

    tower of hanoi

--- python practise/tempCodeRunnerFile.py
+++ python practise/tempCodeRunnerFile.py
@@ -1,18 +1,18 @@
 def convert_weight(weight_kg):
-#     weight_g = weight_kg * 1000
-#     weight_ton = weight_kg / 1000
-#     weight_lb = weight_kg * 2.20462
+    weight_g = weight_kg * 1000
+    weight_ton = weight_kg / 1000
+    weight_lb = weight_kg * 2.20462
 
-#     return weight_g, weight_ton, weight_lb
+    return weight_g, weight_ton, weight_lb
 
-# def main():
-#     weight_kg = float(input("Enter the weight of the commodity in kilograms: "))
+def main():
+    weight_kg = float(input("Enter the weight of the commodity in kilograms: "))
     
-#     weight_g, weight_ton, weight_lb = convert_weight(weight_kg)
+    weight_g, weight_ton, weight_lb = convert_weight(weight_kg)
     
-#     print("The equivalent weight in grams is: {weight_g} g")
-#     print("The equivalent weight in tons is: {weight_ton} tons")
-#     print("The equivalent weight in pounds is: {weight_lb} lbs")
+    print(f"The equivalent weight in grams is: {weight_g:.2f} g")
+    print(f"The equivalent weight in tons is: {weight_ton:.2f} tons")
+    print(f"The equivalent weight in pounds is: {weight_lb:.2f} lbs")
 
-# if __name__ == "__main__":
-#     main()
+if __name__== "__main__":
+    main()
---
+++ recursion_pegs_question.c
@@ -0,0 +1,42 @@
+// #include <stdio.h>
+
+// void hanoi(int n, char source, char target, char auxiliary) {
+//     if (n > 0) {
+//         // Move n - 1 disks from source to auxiliary, so they are out of the way
+//         hanoi(n - 1, source, auxiliary, target);
+
+//         // Move the nth disk from source to target
+//         printf("Move disk %d from peg %c to peg %c\n", n, source, target);
+
+//         // Move the n - 1 disks that we left on auxiliary to target
+//         hanoi(n - 1, auxiliary, target, source);
+//     }
+// }
+
+// int main() {
+//     // initiate call from source A to target C with auxiliary B
+//     hanoi(4, 'A', 'C', 'B');
+//     return 0;
+// }
+
+
+#include <stdio.h>
+
+void hanoi(int n, char source, char target, char auxiliary) {
+    if (n > 0) {
+        // Move n - 1 disks from source to auxiliary, so they are out of the way
+        hanoi(n - 1, source, auxiliary, target);
+
+        // Move the nth disk from source to target
+        printf("Move disk %d from peg %c to peg %c\n", n, source, target);
+
+        // Move the n - 1 disks that we left on auxiliary to target
+        hanoi(n - 1, auxiliary, target, source);
+    }
+}
+
+int main() {
+    // initiate call from source A to target C with auxiliary B
+    hanoi(4, 'A', 'C', 'B');
+    return 0;
+}
