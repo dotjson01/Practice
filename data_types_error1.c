2b547c13f57e02a0f948c142ccca52d7f83adba3
Sahil Ali <sahilali88827@gmail.com>
2024-03-26 22:52:47 +0530

    data types error 1

---
+++ data_types_error1.c
@@ -0,0 +1,16 @@
+// #include<stdio.h>
+
+// int main(){
+//     char ch= 200;
+//     printf("Char is %c\n", ch);
+//     return 0;
+// }
+
+
+#include<stdio.h>
+
+int main(){
+    char ch = 200;
+    printf("Char is %d\n", ch);
+    return 0;
+}
