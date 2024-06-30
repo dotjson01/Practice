fdeba4863b4bf8d2774ff74511ed45eedcee9891
Thestarsahil <sahilali88827@gmail.com>
2024-06-30 14:33:04 +0530

    string

---
+++ string_exercise_b.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+    char s[]="Get Organized ! Learn C!";
+    printf("%s\n",&s[2]);
+    printf("%s\n",s);
+    printf("%s\n",&s);
+    printf("%c\n",s[2]);
+    return 0;
+
+}
