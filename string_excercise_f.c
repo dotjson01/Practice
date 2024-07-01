c4f0a4ca4afd693de6ed490da78e2918c3ef1d99
Thestarsahil <sahilali88827@gmail.com>
2024-07-01 19:52:46 +0530

    2d array

---
+++ string_excercise_f.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+int main(){
+    printf("%d %d %d\n" , sizeof('3'),sizeof("3"),sizeof(3));
+    return 0;
+    
+}
---
+++ string_exercise_e.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main(){
+    printf(5+"Good Morning\n");
+    printf("%c\n","abcdefgh"[4]);
+    return 0;
+
+}
