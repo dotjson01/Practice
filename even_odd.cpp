246f4cb623c0be5de2a52b2ce47a2d26af39ff73
thestarsahil <sahilali88827@gmail.com>
2024-09-12 17:33:25 +0530

    even odd

---
+++ even_odd.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int num;
+    cout<<"Enter the number :"<<endl;
+    cin>>num;
+    if (num%2 == 0){
+        cout<<"Even "<<endl;
+    }
+    else {
+        cout<<"Odd "<<endl;;
+    }
+
+}
