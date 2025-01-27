ce0667c12b8a1ddf2bfb9750a5cb4ae77b267cb6
thestarsahil <sahilali88827@gmail.com>
2025-01-27 17:43:57 +0530

    modified file in 2d_array.cpp

--- 2d_array2.cpp
+++ 2d_array2.cpp
@@ -1,22 +1,39 @@
-#include<iostream>
+#include <iostream>
 
-int main(){
+int main()
+{
 
     int brr[3][3] = {
-        {1,2,3},
-        {4,5,6},
-        {7,8,9}
-    };
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}};
 
-    //row - wise input
-    for(int i =0;i<3;i++){
+//
+std::cout<<"Printing row wise"<<std::endl;
+    // row - wise input
+    for (int i = 0; i < 3; i++)
+    {
 
-        //column wise input
-        for(int j = 0;j<3;j++){
-            std::cout<<brr[i][j]<< " ";
+        for (int j = 0; j < 3; j++)
+        {
+            std::cout << brr[i][j] << " ";
         }
-        std::cout<<std::endl;
+        std::cout << std::endl;
     }
+
+std::cout<<"Printing Column wise"<<std::endl;
+// column wise
+    for (int i = 0; i < 3; i++)
+    {
+
+        // column wise input
+        for (int j = 0; j < 3; j++)
+        {
+            std::cout << brr[j][i] << " ";
+        }
+        std::cout << std::endl;
+    }
+
 
     return 0;
 }
