999e9273d5426dfbc2f9dae9c14a02fd63242223
thestarsahil <sahilali88827@gmail.com>
2025-03-20 10:35:04 +0530

    function

---
+++ functions/pass_by_address.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+void swap(int *a,int *b){ // 
+    int temp =  *a;
+    *a = *b;
+    *b = temp;
+    cout<<"Hello I am Local Stack after executing I' will be deleted"<<endl;
+    cout<<"Value of x: "<<*a<<" Value of y: "<<*b<<endl;
+}
+int main(){
+    int x,y;
+    cout<<"Enter the value of x and y"<<endl;
+    cin>>x>>y;
+    swap(&x,&y); // address are passing to function not values 
+
+    cout<<"Hello I am Main Code after executing I' will be Remain"<<endl;
+    cout<<"Value of x: "<<x<<" Value of y: "<<y<<endl;
+    cout<<"Value of x: "<<&x<<" Value of y: "<<&y<<endl;
+
+
+    cout<<"Hence Proved Pass by Address "<<endl;
+    
+    return 0;
+}
---
+++ functions/pass_by_reference.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+
+void swap(int &a,int &b){
+    int temp = a ;
+    a = b;
+    b = temp;
+    cout<<"Hello I am Local Stack after executing I' will be deleted"<<endl;
+    cout<<"Value of x: "<<a<<" Value of y: "<<b<<endl;
+}
+int main(){
+
+    int x,y;
+    cout<<"Enter the Value of X and Y"<<endl;
+    cin>>x>>y;
+    swap(x,y);
+    cout<<"Hello I am Main Code after executing I' will be Remain"<<endl;
+    cout<<"Value of x: "<<x<<" Value of y: "<<y<<endl;
+
+    cout<<"Hence Proved Pass by Reference actually overwrite the code on a memory"<<endl;
+
+    return 0;
+}
