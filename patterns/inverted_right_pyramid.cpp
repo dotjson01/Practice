1b9a63511528dc1d4eaffc5f0e6c02b2718702fb
thestarsahil <sahilali88827@gmail.com>
2025-07-07 12:31:06 +0530

    sqaure and inverted

--- patterns/inverted_right_pyramid.cpp
+++ patterns/inverted_right_pyramid.cpp
@@ -10,3 +10,44 @@ int main(){
         cout<<endl;
     }
 }
+
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     for(int i=1;i<=5;i++)
+//     {
+//         for(int j=5;j>=i;j--){
+//             cout<<j;
+//         }
+//         cout<<endl;
+//     }
+// }
+
+// OUTPUT
+// 54321
+// 5432
+// 543
+// 54
+// 5
+
+
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     for(int i=1;i<=5;i++)
+//     {
+//         for(int j=5;j>=i;j--){
+//             cout<<i;
+//         }
+//         cout<<endl;
+//     }
+// }
+
+// OUTPUT
+// 11111
+// 2222
+// 333
+// 44
+// 5 
