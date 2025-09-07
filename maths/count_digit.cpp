cb9f31aef25fdb5b53c2ebf989b8180a854db4c2
Dotjson01 <sahilali88827@gmail.com>
2025-09-07 11:19:19 +0530

    palindrome.cpp

--- maths/count_digit.cpp
+++ maths/count_digit.cpp
@@ -1,15 +1,23 @@
 #include<iostream>
 using namespace std;
 
-int main(){
-    int n = 986456;
-    int sum = 0;
-    while(n>0){
-        int lastDigit = n%10;
-        sum += lastDigit;
-        n = n/10;
+ int fun(int num){
+    int sum=0;
+         while(num>0){
+        int lastDigit = num%10;
+         sum += 1;
+        num = num/10;
     }
-    cout<<sum<<endl;
+    return sum;
+    }
+
+int main(){
+    int n;
+    cout<<"Enter the number";
+    cin>>n;
+    int count=fun(n);
+   
+    cout<<count<<endl;
     return 0;
     
 }
