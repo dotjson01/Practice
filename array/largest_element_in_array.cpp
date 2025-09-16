efc20a8970b1ab05c8759466fadfb9dce69cf190
Dotjson01 <sahilali88827@gmail.com>
2025-09-16 08:51:25 +0530

    largest element in vector

--- array/largest_element_in_array.cpp
+++ array/largest_element_in_array.cpp
@@ -6,9 +6,27 @@ using namespace std;
 
 int main(){
 
+    int stored_largest = INT_MIN ;                            // - 2^n to 2^n-1  - 1 
+    int stored_smallest = INT_MAX;                            // 2^n-1  -1 to 2^n
+
     vector<int> element = { 21, 43, 102, 65, 50, 2, 100 };
     cout<<"Vector Size "<<element.size()<<endl;
+    cout<<"Before Implementation "<<endl;
+
     for(int i = 0 ; i < element.size(); i++){
         cout<<i<<" at "<<element[i]<<endl;
     }
+    cout<<"After Implementation Finding largest value in a element "<<endl;
+
+    for(int i=0 ; i<element.size() ; i++){
+        if(element[i] > stored_largest){
+            stored_largest = element[i];
+        }
+        if(element[i] < stored_smallest){
+            stored_smallest  = element[i] ;
+        }
+    }
+
+    cout<<stored_largest<<endl;
+    cout<<stored_smallest<<endl;
 }
