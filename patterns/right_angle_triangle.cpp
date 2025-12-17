5f4ca461312959727148af9f9767d373351545e3
Dotjson01 <sahilali88827@gmail.com>
2025-12-17 17:45:49 +0530

    for loop , right angle triangle, square

--- patterns/right_angle_triangle.cpp
+++ patterns/right_angle_triangle.cpp
@@ -2,11 +2,7 @@
 using namespace std;
 
 int main(){
-
-    for (int i=0;i<=5;i++){
-        for(int j=0;j<=i;j++){
-            cout<<"*";
-        }
-        cout<<endl;
-    }
+    int rows,cols;
+    cin>>rows>>cols;
+    for(int )
 }
--- patterns/square.cpp
+++ patterns/square.cpp
@@ -2,11 +2,20 @@
 using namespace std;
 
 int main(){
-    int n =3 ;
-    for (int i=1;i<=n;i++){
-        for(int j=1;j<=n;j++){
-            cout<<j;
+    int n =7 ;
+    for (int i=1;i<=n;i++){ //rows : runs once per output row each, each iteration begins a new line
+        for(int j=1;j<=n;j++){ // columns: runs inside the current outer iteration to print characters for the current row
+            // it repeats printing stars till n 
+            // condition break 
+            // loop ended
+            cout<<"*";
         }
+        // fall out and print new line 
         cout<<endl;
     }
+    // again goes to outer loop because it doesn't ended yet till n 
+    // now i = 2, loop runs , condition check 
+    // fall into j loop 
+    //j=1 to j <= n runs again 
+    // print star
 }
