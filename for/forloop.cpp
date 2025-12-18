c1ede44222dae8ca235a9fc51ef16e3a2da6cc0e
Dotjson01 <sahilali88827@gmail.com>
2025-12-18 23:19:14 +0530

    forloop.cpp

--- for/forloop.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-
-int main(){
-
-    // i want , my name shoudl print 10 times 
-    for(int i = 1; i < 10 ; i++){
-        cout<<"My name is DOTJSON"<<endl;
-    }}
