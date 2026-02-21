de0d65851af67f7943f38d49bedb1a9ef436ee23
Dotjson01 <sahilali88827@gmail.com>
2026-02-21 23:05:16 +0530

    program1.cpp

--- BegineerLevel/program1.cpp
+++ BegineerLevel/program1.cpp
@@ -6,7 +6,6 @@ int main()
     std::cout << "Enter two numbers:" << std::endl;
     int v1, v2;
     std::cin >> v1 >> v2;
-    std::cout << "The sum of " << v1 << " and " << v2
-              << " is " << v1 + v2 << std::endl;
+    std::cout << "The sum of " << v1 << " and " << v2 << " is " << v1 + v2 << std::endl;
     return 0;
 }
