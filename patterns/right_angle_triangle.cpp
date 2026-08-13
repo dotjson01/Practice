9ba7cde7aa0d8f563c77c4827cb7752c6d876be5
Dotjson01 <sahilali88827@gmail.com>
2026-08-13 22:15:41 +0530

    right angle triangle

--- patterns/right_angle_triangle.cpp
+++ patterns/right_angle_triangle.cpp
@@ -1,7 +1,32 @@
 #include<iostream>
 using namespace std;
 
+
+/*
+
+
+ *
+ **
+ ***
+ ****
+
+*/
+
+
+
+void fun(int n ){
+    for (int i = 0; i < n ; i ++ ){
+        for ( int j = 0 ; j < i + 1 ; j ++){
+            cout << " * " ;
+        }
+        cout <<endl;
+    }
+
+}
+
 int main(){
-    int rows,cols;
-    cin>>rows>>cols;
+    int n ;
+    cout << " Enter the number of lines" ;
+    cin >> n;
+    fun(n);
 }
