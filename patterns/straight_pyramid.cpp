63e9d26636aa8fd4a5405d3d7211c744b9e61ddc
Dotjson01 <sahilali88827@gmail.com>
2026-08-14 23:07:48 +0530

    straight pyramid

---
+++ patterns/straight_pyramid.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+void fun(int n)
+{
+
+    // outer loop for rows
+    for (int i = 0; i < n; i++)
+    {
+        // inner loop for space
+        for (int j = 0; j <= n - 1-i; j++)
+        {
+            cout << " ";
+        }
+        // loop inside inner loop
+        for (int k = 1; k <= 2 * i + 1; k++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    int n;
+    cout << "Enter the number";
+    cin >> n;
+    fun(n);
+}
