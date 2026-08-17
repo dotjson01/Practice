7de9556f3f5031a7940ac884a2e345b2dde2457a
Dotjson01 <sahilali88827@gmail.com>
2026-08-17 22:57:00 +0530

    sorted

--- Sorting/Selection_Sort.cpp
+++ Sorting/Selection_Sort.cpp
@@ -2,5 +2,12 @@
 using namespace std;
 
 int main(){
-    
+    int arr[] = {13,14,24,52,20};
+    int n = size(arr) / sizeof(arr[0]);
+
+    // print array
+    cout<<"Before Selection " << "\n";
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<< " ";
+    }
 }
